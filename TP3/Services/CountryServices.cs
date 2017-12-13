e50480c636febe142e649d5e89da8a4d38ed5e8f
MiguelSerki <miguel.serki@gmail.com>
2017-12-13 09:17:27 -0300

    Interfaz de operaciones CRUD. Servicios de empleados y paises (vacio)

---
+++ TP3/Services/CountryServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.DTO;
+
+namespace Services
+{
+    public class CountryServices : ICrud<CountryDTO>
+    {
+        public CountryDTO Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(CountryDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CountryDTO> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read(CountryDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CountryDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TP3/Services/EmployeeServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.DTO;
+
+namespace Services
+{
+    public class EmployeeServices : ICrud<EmployeeDTO>
+    {
+        public EmployeeDTO Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(EmployeeDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmployeeDTO> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read(EmployeeDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(EmployeeDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
