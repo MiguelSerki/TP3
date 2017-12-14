d8a2ed4417e7a2fe75772962792d57a2985b6cb7
AGUSTINBIGL681B\agustin <aguspicle@hotmail.com>
2017-12-13 21:13:05 -0300

    CRUD Empleados

---
+++ TP3/Services/EmployeesServices.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Services.DTO;
+
+namespace Services
+{
+    class EmployeesServices
+    {
+        private Repository<Employee> _EmployeeRepository;
+
+        public EmployeesServices()
+        {
+            _EmployeeRepository = new Repository<Employee>();
+        }
+
+        public IEnumerable<EmployeeDTO> GetAll()
+            {
+            return _EmployeeRepository.Set().ToList().Select(c => new EmployeeDTO
+            {
+                EmployeeID = c.EmployeeID,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Country = c.Country,
+                Shift = c.Shift
+            });
+            }
+
+        public void Create(EmployeeDTO dto)
+        {
+            _EmployeeRepository.Persist(new Employee
+            {
+                EmployeeID = dto.EmployeeID,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                Shift = dto.Shift,
+                Country = dto.Country,
+                HireDate = dto.HireDate
+            });
+
+            _EmployeeRepository.SaveChanges();
+        }
+
+            
+        public void Update(EmployeeDTO dto)
+        {
+            var employee = _EmployeeRepository.Set().FirstOrDefault(x => x.EmployeeID == dto.EmployeeID);
+            if(employee == null)
+            {
+                throw new Exception("El empleado a actualizar no existe");
+
+
+            }
+
+            employee.FirstName = dto.FirstName;
+            employee.LastName = dto.LastName;
+            employee.Country = dto.Country;
+            employee.Shift = dto.Shift;
+            employee.HireDate = dto.HireDate;
+            
+
+            _EmployeeRepository.Update(employee);
+            _EmployeeRepository.SaveChanges();
+
+            
+
+
+        }
+
+        public void Delete(EmployeeDTO dto)
+
+        {
+            var employee = _EmployeeRepository.Set().FirstOrDefault(x => x.EmployeeID == dto.EmployeeID):
+
+            if(employee==null)
+
+            {
+                throw new Exception("El empleado a eliminar no existe");
+
+            }
+
+            _EmployeeRepository.Remove(employee);
+            _EmployeeRepository.SaveChanges();
+
+        }
+
+           
+            
+                
+            
+        
+        
+
+
+
+    }
+}
